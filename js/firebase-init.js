feb375e012bd005d54b98721be8511a4a91d79fc
emartinie <eddie.martinie@gmail.com>
2026-04-24 10:45:38 -0400

    Update Firebase configuration in firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
+  apiKey: "AIzaSyATCjiScxnuuZe6bgd1KP5p2l0dVs0B8bQ",
+  authDomain: "homegroups-app.firebaseapp.com",
   projectId: "homegroups-app",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  storageBucket: "homegroups-app.firebasestorage.app",
+  messagingSenderId: "776120047000",
+  appId: "1:776120047000:web:c1ee1e179adc4f970fc7ed",
+  measurementId: "G-3BJR8ZZ2EY"
 };
 
 const app = initializeApp(firebaseConfig);
