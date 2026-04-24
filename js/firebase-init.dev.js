d440f27ff875424ebee30c137373313ea1dc389d
emartinie <eddie.martinie@gmail.com>
2026-04-24 10:26:07 -0400

    Refactor Firebase initialization and update SDK version
    Updated Firebase SDK version and refactored initialization code.

--- js/firebase-init.dev.js
+++ js/firebase-init.dev.js
@@ -1,35 +1,14 @@
-// firebase-init.dev.js
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-import {
-  getFirestore,
-  collection,
-  onSnapshot,
-  query,
-  orderBy
-}
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-console.log("🔥 firebase-init.dev.js loading");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNYsljwgrRaJNycyNivNUbY6DPteYNyag",
-  authDomain: "prayermap-669fc.firebaseapp.com",
-  projectId: "prayermap-669fc",
-  storageBucket: "prayermap-669fc.appspot.com",
-  messagingSenderId: "887582168434",
-  appId: "1:887582168434:web:2bccf2d4e1b9e79721f9eb"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "homegroups-app",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// expose globals for non-module card scripts
-window.db = db;
-window.collection = collection;
-window.onSnapshot = onSnapshot;
-window.query = query;
-window.orderBy = orderBy;
-
-console.log("🔥 Firebase initialized");
+export const db = getFirestore(app);
