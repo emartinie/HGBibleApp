f4025c0cbc4763d2cc45951d10b5666faf2926e0
emartinie <eddie.martinie@gmail.com>
2026-05-04 00:00:07 -0400

    Refactor app.js to improve structure and readability

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 console.log("🔥 app.js START");
 
-//import { initializeTimeSystem } from "./firebaseTimeLoader.js";
+import { initializeTimeSystem } from "./firebaseTimeLoader.js";
 //import { TimeStore } from "./timeStore.js";
 //import { initializeCardRenderer } from "./cardRenderer.js";
 
 console.time("BOOT TRACE");
 
-//await initializeTimeSystem();
+await initializeTimeSystem();
 console.log("✔ time system ready");
 
 //initializeCardRenderer();
