cb52254cbb1a77a6966f8b210106c1555bca470b
emartinie <eddie.martinie@gmail.com>
2026-05-17 22:59:51 -0400

    Add JOURNEY_INDEX and loadJourney function

--- js/journeys.js
+++ js/journeys.js
@@ -1,3 +1,26 @@
+const JOURNEY_INDEX = [
+  {
+    id: "learn-to-pray",
+    file: "data/journeys/learn-to-pray.json"
+  },
+  {
+    id: "27-things-seminary",
+    file: "data/journeys/27-things.json"
+  }
+];
+
+async function loadJourney(id) {
+  const meta = JOURNEY_INDEX.find(j => j.id === id);
+  if (!meta) throw new Error("Journey not found");
+
+  const res = await fetch(meta.file);
+  return await res.json();
+}
+
+const journey = await loadJourney("learn-to-pray");
+
+renderJourney(journey, state);
+
 function renderJourney(journey, state) {
   const progress = state.progress?.[journey.id] || {
     completedSteps: [],
